17fb4d1a4c392bb2f2705b5510c7c5d9a6472134
Guillaume <guillaume_proulx@outlook.com>
2019-10-13 13:43:33 -0400

    clean du code pour remise final

--- tp2/tp2_Maps/src/tp2/Interview.java
+++ tp2/tp2_Maps/src/tp2/Interview.java
@@ -16,59 +16,62 @@ public class Interview {
      * @return A collection containing all valid pairs with no permutations, but all combinations (empty collection if none found)
      */
 
-// methode par hashage
-    public Collection<MatchingPair> matchingPairs(Collection<Integer> values, Integer targetSum) {
+    // methode par hashage
+    public Collection<MatchingPair> matchingPairs(Collection<Integer> values, Integer targetSum)
+    {
         HashMap<Integer, Integer> tableCompteur = new HashMap<>();
         LinkedHashMap<Integer, Integer> tablePaire = new LinkedHashMap<Integer, Integer>();
         Collection<MatchingPair> paireSansDuplication = new LinkedList();
         Collection<MatchingPair> solution = new LinkedList();
 
         // On itere sur les valeurs donnee pour savoir le compte de chaque valeur
-        for (Integer valeur : values) {
-            if (tableCompteur.containsKey(valeur)) {
-
-                    // Partie Compteur
-                    Integer compteur = tableCompteur.get(valeur);
-                    compteur++;
-                    tableCompteur.put(valeur, compteur);
+        for (Integer valeur : values)
+        {
+            if (tableCompteur.containsKey(valeur))
+            {
+                // Partie Compteur
+                Integer compteur = tableCompteur.get(valeur);
+                compteur++;
+                tableCompteur.put(valeur, compteur);
             }
-                else {
-
-                    tableCompteur.put(valeur, 0);
-                }
+            else {
+                tableCompteur.put(valeur, 0);
+            }
 
         }
 
-// On itere sur les valeurs donnee pour savoir les paires
-        for (Integer element : values) {
+        // On itere sur les valeurs donnee pour savoir les paires
+        for (Integer element : values)
+        {
             int temp = targetSum - element;
-            if (tablePaire.containsKey(element)) {
-                if (tablePaire.get(element) != null) {
+            if (tablePaire.containsKey(element))
+            {
+                if (tablePaire.get(element) != null)
+                {
                     paireSansDuplication.add(new MatchingPair(element, temp));
-
                 }
                 // si la table contient l'element on met nul pour ne pas avoir de repetition
                 tablePaire.put(temp, null);
-            } else if (!tablePaire.containsKey(element)) {
+            }
+            else if (!tablePaire.containsKey(element))
+            {
                 tablePaire.put(temp, element);
             }
         }
 
         // On trouve le minimum count entre les paire et leur compte pour tenir en compte de toutes les possibilites. Ici le meilleur cas reste O(n) ( pas de paire)
-        for (MatchingPair paire : paireSansDuplication) {
+        for (MatchingPair paire : paireSansDuplication)
+        {
             if (paire != null) { // verification au debug pr erreur nullpointer
                 int a = tableCompteur.get(paire.first);
                 int b = tableCompteur.get(paire.second);
                 int minCount = Math.min(a+1, b+1);
-                for (int i = 0; i < pow(minCount, 2); i++) {
+                for (int i = 0; i < pow(minCount, 2); i++)
+                {
                     solution.add(new MatchingPair(paire.first, paire.second));
                 }
             }
-
-
-
         }
-        System.out.println(solution.size());
         return solution;
     }
 }
